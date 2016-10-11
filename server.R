b1930cac4e9bac0f396e7670c9d44dc9e90989d7
Alex Lugo <alugocp@aim.com>
2016-10-11 11:05:06 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -1,31 +1,50 @@
 library(shiny)
 library(survival)
+#data <- read.table(pipe("ssh alugocp@ibissub01@umiacs.umd.edu '../../cbcb/project2-scratch/gi_visualization/validation.data.mRNA.RData'"),header=T)
 shinyServer(function(input,output){
-	output$selectedGraph <- renderPlot({
-    		if(input$gender==T){
-    			gender()
-    		}else{
-    			one()
-    		}
-    	})
-    	one <- function(){
-    		lung$SurvObj <- with(lung,Surv(time,status==2))
-		km <- survfit(SurvObj ~1, data = lung, conf.type = "log-log")
-		plot(km,conf=F,mark.time=F)
-    	}
-    	gender <- function(){
-    		data <- split(lung,lung$sex)
-    		lung1 <- data$"1"
-    		lung2 <- data$"2"
-    			
-    		#boys
-		lung1$SurvObj <- with(lung1,Surv(time,status==2))
-		km <- survfit(SurvObj ~1, data = lung1, conf.type = "log-log")
-		plot(km,conf=F,mark.time=F,col="blue")
-			
-		#girls
-		lung2$SurvObj <- with(lung2,Surv(time,status==2))
-		km <- survfit(SurvObj ~ 1, data = lung2, conf.type = "log-log")
-		lines(km,conf=F,mark.time=F,col="pink")
-    	}
+	output$km <- renderPlot({
+		if(input$gene1=="none"){
+			graphNode(input$quantile,input$gene)
+		}else{
+			graphEdge(input$quantile,input$gene,input$gene1)
+		}
+	})
+	graphNode <- function(k,gene){
+		data <- lung
+		cutoffs <- quantile(data[,gene],c(k,1-k),na.rm=TRUE)#na.rm=TRUE allows for N/A data. Remove later?
+		
+		low <- subset(data,data[gene]<=cutoffs[1])
+		low$SurvObj <- with(low,Surv(time,status==2))
+		km <- survfit(SurvObj~1,data=low,conf.type="log-log")
+		plot(km,conf=F,mark.time=F,col="green")
+		
+		high <- subset(data,data[gene]>=cutoffs[2])
+		high$SurvObj <- with(high,Surv(time,status==2))
+		km <- survfit(SurvObj~1,data=high,conf.type="log-log")
+		lines(km,conf=F,mark.time=F)
+	}
+	graphEdge <- function(k,gene,gene1){
+		data <- lung
+		cutoffs <- c(quantile(data[,gene],c(k,1-k),na.rm=TRUE),quantile(data[,gene1],c(k,1-k),na.rm=TRUE))
+		
+		lowlow <- subset(data,data[gene]<=cutoffs[1] & data[gene1]<=cutoffs[3])
+		lowlow$SurvObj <- with(lowlow,Surv(time,status==2))
+		km <- survfit(SurvObj~1,data=lowlow,conf.type="log-log")
+		plot(km,conf=F,mark.time=F,col="red")
+		
+		lowhigh <- subset(data,data[gene]<=cutoffs[1] & data[gene1]>=cutoffs[4])
+		lowhigh$SurvObj <- with(lowhigh,Surv(time,status==2))
+		km <- survfit(SurvObj~1,data=lowhigh,conf.type="log-log")
+		lines(km,conf=F,mark.time=F,col="blue")
+		
+		highhigh <- subset(data,data[gene]>=cutoffs[2] & data[gene1]>=cutoffs[4])
+		highhigh$SurvObj <- with(highhigh,Surv(time,status==2))
+		km <- survfit(SurvObj~1,data=highhigh,conf.type="log-log")
+		lines(km,conf=F,mark.time=F,col="green")
+		
+		highlow <- subset(data,data[gene]>=cutoffs[2] & data[gene1]<=cutoffs[3])
+		highlow$SurvObj <- with(highlow,Surv(time,status==2))
+		km <- survfit(SurvObj~1,data=highlow,conf.type="log-log")
+		lines(km,conf=F,mark.time=F)
+	}
 })
--- ui.R
+++ ui.R
@@ -1,19 +1,17 @@
 library(shiny)
 shinyUI(fluidPage(
-	tags$title("Kaplan-Meier Estimator"),
+	tags$title("Alex Lugo's Kaplan-Meier Estimator"),
 	fluidRow(
 		column(3,
-			tags$h2("Hi"),
-			"What's up?",
-			br(),
-			"Look what I made let's goooooOOOOOooooo",
-			br(),
+			tags$h2("KM Plotter"),
+			selectInput("gene","Pick a factor",choices=list("Age"="age","Weight Loss"="wt.loss","Meal Calories"="meal.cal","Gender"="sex")),
+			selectInput("gene1","Pick a factor (optional)",choices=list("-Select-"="none","Age"="age","Weight Loss"="wt.loss","Meal Calories"="meal.cal","Gender"="sex")),
+			numericInput("quantile","Pick a K value",0.1,step=0.05,min=0.1,max=0.5),
 			submitButton("Eyyy a button"),
 			tags$input(type="submit",onclick="alert('hi')",value="hey")
 		),
 		mainPanel(
-			checkboxInput("gender","Show by Sex?"),
-			plotOutput("selectedGraph")
+			plotOutput("km")
 		)
 	),fluidRow(
 		column(12,
