7baf5127f396fed87783177f6a7e2bfaa9dd9fc3
Alex Lugo <alugocp@aim.com>
2016-11-18 20:27:20 -0500

    Add files via upload

--- www/frontEnd.js
+++ www/frontEnd.js
@@ -4,6 +4,7 @@ var c=canvas.getContext("2d");
 
 //engine support
 var nodes=Array();
+var edges=Array();
 var selected=null;
 var mouse=null;
 function formatClick(e){
@@ -16,7 +17,6 @@ function mousedown(e){
 	for(var a=nodes.length-1;a>=0;a--){
 		if(distance(nodes[a],e.x,e.y)<nodes[a].radius){
 			selected=nodes[a];
-			//lastmouse=[e.x,e.y];
 			return;
 		}
 	}
@@ -32,7 +32,7 @@ function mousemove(e){
 function clickCanvas(){
 	var x=mouse[0];
 	var y=mouse[1];
-	for(var a=0;a<nodes.length;a++){
+	for(var a=nodes.length-1;a>=0;a--){
 		var node=nodes[a];
 		if(distance(node,x,y)<=node.radius){
 			Shiny.onInputChange("gene",node.name);
@@ -40,27 +40,20 @@ function clickCanvas(){
 			return;
 		}
 	}
-	for(var a=0;a<nodes.length;a++){
-		for(var b=0;b<a;b++){
-			if(edgeClick(nodes[a],nodes[b])){
-				return;
-			}
-		}
-		for(var b=a+1;b<nodes.length;b++){
-			if(edgeClick(nodes[a],nodes[b])){
-				return;
-			}
+	for(var a=edges.length-1;a>=0;a--){
+		if(edgeClick(nodes[edges[a].start],nodes[edges[a].end],edges[a].width)){
+			return;
 		}
 	}
 }
-function edgeClick(node,node1){
+function edgeClick(node,node1,width){
 	var x=mouse[0];
 	var y=mouse[1];
 	if((x>node.x)!=(x>node1.x) || (node.x==node1.x)){
 		var m=(node.y-node1.y)/(node.x-node1.x);
 		if(Math.abs(m)==Infinity){
 			if((y>node.y)!=(y>node1.y)){
-				if(Math.abs(x-node.x)<Node.edge){
+				if(Math.abs(x-node.x)<width){
 					Shiny.onInputChange("gene",node.name);
 					Shiny.onInputChange("gene1",node1.name);
 					return true;
@@ -74,7 +67,7 @@ function edgeClick(node,node1){
 			}else{
 				expectedY=(m*(x-node1.x))+node1.y;
 			}
-			var leniency=Math.abs(Node.edge/Math.cos(Math.atan(m)));
+			var leniency=Math.abs(width/Math.cos(Math.atan(m)));
 			if(Math.abs(y-expectedY)<leniency){
 				Shiny.onInputChange("gene",node.name);
 				Shiny.onInputChange("gene1",node1.name);
@@ -100,19 +93,19 @@ function distance(){
 }
 
 //class definitions
-function Node(name,x,y){
-	Node.edge=7;
-	this.radius=15;//(c.measureText(name).width/2)+5;//36
-	this.x=x;
-	this.y=y;
+function Node(name,color){
+	this.radius=15;
 	this.name=name;
 	this.color=getColor(name);
-	this.connections=new Array();
-	if(arguments.length>3){
-		for(var a=3;a<arguments.length;a++){
-			this.connections.push(arguments[a]);
-		}
-	}
+	this.x=0;
+	this.y=0;
+}
+function Edge(color,width,sign,start,end){
+	this.color=color;
+	this.width=width;
+	this.sign=sign;
+	this.start=start;
+	this.end=end;
 }
 
 //nodes support
@@ -130,32 +123,10 @@ function getColor(name){
 	}
 	return "#"+color;
 }
-function setEdges(){
-	for(var a=0;a<nodes.length;a++){
-		for(var b=0;b<nodes[a].connections.length;b++){
-			var found=false;
-			for(var c=0;c<nodes.length;c++){
-				if(nodes[c].name==nodes[a].connections[b]){
-					nodes[a].connections[b]=c;
-					if(b>0){
-						nodes[a].radius+=5;
-					}
-					found=true;
-				}
-			}
-			if(!found){
-				nodes[a].connections.splice(b,1);
-				b--;
-			}
-		}
-	}
-}
-function getCoordinates(index,length){
-	if(index==0){
-		return "650,170";
-	}
-	var angle=(index-1)*(Math.PI*2)/length;
-	return Math.round(650+(100*Math.cos(angle)))+","+Math.round(170+(100*Math.sin(angle)));
+function setCoordinates(node,index,length){
+	var angle=index*(Math.PI*2)/length;
+	node.x=Math.round(650+(100*Math.cos(angle)));
+	node.y=Math.round(170+(100*Math.sin(angle)));
 }
 var nodeData="";
 function populateNodes(){
@@ -163,27 +134,69 @@ function populateNodes(){
 	while(nodes.length>0){
 		nodes.splice(0,1);
 	}
-	var information=nodeData.split("[1]");
+	while(edges.length>0){
+		edges.splice(0,1);
+	}
+	var data=nodeData;
+	for(var a=0;a<data.length;a++){
+		if(data.substring(a,a+1)=="\"" || data.substring(a,a+1)=="\n" || data.substring(a,a+1)==" "){
+			data=data.substring(0,a)+data.substring(a+1);
+			a--;
+		}
+	}
+	if(data.substring(0,6)=="[1]NA,"){
+		return;
+	}
+	
+	information=data.split("[1]");
 	information.splice(0,1);
-	for(var i=0;i<information.length;i++){
-		var data=information[i];
-		for(var a=0;a<data.length;a++){
-			if(data.substring(a,a+1)==" " || data.substring(a,a+1)=="\"" || data.substring(a,a+1)=="\n"){
-				data=data.substring(0,a)+data.substring(a+1);
-				a--;
+	for(var a=0;a<information.length;a++){
+		information[a]=information[a].split(",");
+		var add=true;
+		for(var b=0;b<nodes.length;b++){
+			if(nodes[b].name==information[a][0]){
+				add=false;
+				break;
 			}
 		}
-		data=data.split(",");
-		console.log(data);
-		var construct="nodes.push(new Node(\""+data[0]+"\","+getCoordinates(i,information.length-1);
-		for(var a=1;a<data.length;a++){
-			construct+=",\""+data[a]+"\"";
+		if(add==true){
+			nodes.push(new Node(information[a][0],information[a][1]));
 		}
-		construct+="));";
-		console.log(construct);
-		eval(construct);
 	}
-	setEdges();
+	for(var a=0;a<information.length;a++){
+		for(var b=3;b<information[a].length;b+=4){
+			establishConnection(information[a][0],information[a][b],information[a][b+1],information[a][b+2],information[a][b+3]);
+		}
+	}
+	for(var a=0;a<nodes.length;a++){
+		setCoordinates(nodes[a],a,nodes.length);
+	}
+}
+function establishConnection(gene1,gene2,color,width,sign){
+	for(var a=0;a<edges.length;a++){
+		if((edges[a].start==gene1 && edges[a].end==gene2) || (edges[a].start==gene2 && edges[a].end==gene1)){
+			return;
+		}
+	}
+	var index1=-1;
+	var index2=-1;
+	for(var a=0;a<nodes.length;a++){
+		if(nodes[a].name==gene1){
+			index1=a;
+			break;
+		}
+	}
+	for(var a=0;a<nodes.length;a++){
+		if(nodes[a].name==gene2){
+			index2=a;
+			break;
+		}
+	}
+	if(index1>=0 && index2>=0){
+		edges.push(new Edge(color,width,sign,index1,index2));
+		nodes[index1].radius+=5;
+		nodes[index2].radius+=5;
+	}
 }
 
 //drawing
@@ -223,18 +236,21 @@ function drawNodes(){
 	}
 }
 function drawEdges(){
-	c.lineWidth=Node.edge;
-	c.strokeStyle="gray";
-	for(var a=0;a<nodes.length;a++){
-		var node=nodes[a];
-		for(var b=0;b<nodes[a].connections.length;b++){
-			var node1=nodes[nodes[a].connections[b]];
-			c.beginPath();
-			c.moveTo(node.x,node.y);
-			c.lineTo(node1.x,node1.y);
-			c.stroke();
-			c.closePath();
-		}
+	for(var a=0;a<edges.length;a++){
+		var edge=edges[a];
+		c.strokeStyle=edge.color;
+		c.lineWidth=edge.width;
+		c.beginPath();
+		var startX=nodes[edge.start].x;
+		var startY=nodes[edge.start].y;
+		var endX=nodes[edge.end].x;
+		var endY=nodes[edge.end].y;
+		c.moveTo(startX,startY)
+		c.lineTo(endX,endY)
+		c.stroke();
+		c.closePath();
+		c.fillStyle="black";
+		c.fillText(edge.sign,((endX-startX)/2)+startX,((endY-startY)/2)+startY);
 	}
 }
 function drawHelpButton(){
@@ -272,7 +288,7 @@ function setCanvasDimensions(){
 	canvas.height=window.innerHeight-rect.top-rect.left;
 }
 function update(){
-	setTimeout(update,100);
+	setTimeout(update,250);
 	if(image==undefined){
 		image=$("img")[0];
 		return;
@@ -291,5 +307,4 @@ function update(){
 	drawHelpButton();
 }
 setCanvasDimensions();
-setEdges();
 update();
