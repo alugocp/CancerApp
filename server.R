5e91190763308f653e45b4addac2f4b349b645de
Alex Lugo <alugocp@aim.com>
2016-12-14 10:33:22 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -30,7 +30,7 @@ shinyServer(function(input,output){
 		km <- survfit(clin~1,data=d,conf.type="log-log")
 		lines(km,conf.int=F,mark.time=F,col="green")
 		
-		rm(d,clin,km)
+		rm(d,clin,km,bins)
 		legend(1,0.4,c("low","med","high"),col=c("red","blue","green"),lty=c(1,1))
 	}
 	graphEdge <- function(gene,gene1){
@@ -40,55 +40,63 @@ shinyServer(function(input,output){
 		flipbins <- (bin.map[i1,]*3)+bin.map[i,]+1
 		graphData(paste(gene," vs. ",gene1))
 		
-		bin1 <- data.frame(subset(data,bins[]==1))
-		bin1clin <- with(bin1,Surv(time,status==1))
-		km <- survfit(bin1clin~1,data=bin1,conf.type="log-log")
+		bin <- data.frame(subset(data,bins[]==1))
+		clin <- with(bin,Surv(time,status==1))
+		km <- survfit(clin~1,data=bin,conf.type="log-log")
 		lines(km,conf.int=F,mark.time=F,col="red")
 		
-		bin2 <- data.frame(subset(data,bins[]==2))
-		bin2clin <- with(bin2,Surv(time,status==1))
-		km <- survfit(bin2clin~1,data=bin2,conf.type="log-log")
+		bin <- data.frame(subset(data,bins[]==2))
+		clin <- with(bin,Surv(time,status==1))
+		km <- survfit(clin~1,data=bin,conf.type="log-log")
 		lines(km,conf.int=F,mark.time=F,col="yellow")
 		
-		bin3 <- data.frame(subset(data,bins[]==3))
-		bin3clin <- with(bin3,Surv(time,status==1))
-		km <- survfit(bin3clin~1,data=bin3,conf.type="log-log")
+		bin <- data.frame(subset(data,bins[]==3))
+		clin <- with(bin,Surv(time,status==1))
+		km <- survfit(clin~1,data=bin,conf.type="log-log")
 		lines(km,conf.int=F,mark.time=F,col="orange")
 		
-		bin4 <- data.frame(subset(data,flipbins[]==2))
-		bin4clin <- with(bin4,Surv(time,status==1))
-		km <- survfit(bin4clin~1,data=bin4,conf.type="log-log")
+		bin <- data.frame(subset(data,flipbins[]==2))
+		clin <- with(bin,Surv(time,status==1))
+		km <- survfit(clin~1,data=bin,conf.type="log-log")
 		lines(km,conf.int=F,mark.time=F,col="purple")
 		
-		bin5 <- data.frame(subset(data,bins[]==5))
-		bin5clin <- with(bin5,Surv(time,status==1))
-		km <- survfit(bin5clin~1,data=bin5,conf.type="log-log")
+		bin <- data.frame(subset(data,bins[]==5))
+		clin <- with(bin,Surv(time,status==1))
+		km <- survfit(clin~1,data=bin,conf.type="log-log")
 		lines(km,conf.int=F,mark.time=F,col="blue")
 		
-		bin6 <- data.frame(subset(data,bins[]==6))
-		bin6clin <- with(bin6,Surv(time,status==1))
-		km <- survfit(bin6clin~1,data=bin6,conf.type="log-log")
+		bin <- data.frame(subset(data,bins[]==6))
+		clin <- with(bin,Surv(time,status==1))
+		km <- survfit(clin~1,data=bin,conf.type="log-log")
 		lines(km,conf.int=F,mark.time=F,col="pink")
 		
-		bin7 <- data.frame(subset(data,flipbins[]==3))
-		bin7clin <- with(bin7,Surv(time,status==1))
-		km <- survfit(bin7clin~1,data=bin7,conf.type="log-log")
+		bin <- data.frame(subset(data,flipbins[]==3))
+		clin <- with(bin,Surv(time,status==1))
+		km <- survfit(clin~1,data=bin,conf.type="log-log")
 		lines(km,conf.int=F,mark.time=F,col="gray")
 		
-		bin8 <- data.frame(subset(data,flipbins[]==6))
-		bin8clin <- with(bin8,Surv(time,status==1))
-		km <- survfit(bin8clin~1,data=bin8,conf.type="log-log")
+		bin <- data.frame(subset(data,flipbins[]==6))
+		clin <- with(bin,Surv(time,status==1))
+		km <- survfit(clin~1,data=bin,conf.type="log-log")
 		lines(km,conf.int=F,mark.time=F,col="cyan")
 		
-		bin9 <- data.frame(subset(data,bins[]==9))
-		bin9clin <- with(bin9,Surv(time,status==1))
-		km <- survfit(bin9clin~1,data=bin9,conf.type="log-log")
+		bin <- data.frame(subset(data,bins[]==9))
+		clin <- with(bin,Surv(time,status==1))
+		km <- survfit(clin~1,data=bin,conf.type="log-log")
 		lines(km,conf.int=F,mark.time=F,col="green")
 		
+		rm(bin,clin,km,bins,flipbins)
 		legend(1,0.6,c("low-low","low-med","low-high","med-low","med-med","med-high","high-low","high-med","high-high"),col=c("red","yellow","orange","purple","blue","pink","gray","cyan","green"),lty=c(1,1))
 	}
 	graphData <- function(label){
-		plot(fullKm,conf.int=F,mark.time=F,xlab="Time",ylab="Chance of Survival",main=label)
+		plot(fullKm,conf.int=F,mark.time=F,xlab="Time (Days)",ylab="Chance of Survival",main=label)
+	}
+	getRole <- function(index){
+		i <- match(genes[index],roles[,1])
+		if(!is.na(i)){
+			return(roles[i,2])
+		}
+		return("none")
 	}
 	flip <- function(bin){
 		if(bin==2){
@@ -111,7 +119,7 @@ shinyServer(function(input,output){
 			index <- match(searched[s],genes)
 			if(!is.na(index)){
 				con <- subset(interactions,interactions[,"y"]==index | interactions[,"x"]==index)
-				x <- paste(searched[s],"NULL","true",sep=",")
+				x <- paste(searched[s],getRole(index),"true",sep=",")
 				if(length(con[,1])==0){
 					print(x)
 				}else{
@@ -138,7 +146,7 @@ shinyServer(function(input,output){
 							index1 <- con[i,"y"]
 						}
 						con1 <- subset(interactions,interactions[,"y"]==index1 | interactions[,"x"]==index1)
-						x1 <- paste(genes[index1],"NULL","false",sep=",")
+						x1 <- paste(genes[index1],getRole(index1),"false",sep=",")
 						if(length(con1[,1])>0){
 							for(a in 1:length(con1[,1])){
 								if(con1[a,"y"]==index1){
--- startApp.R
+++ startApp.R
@@ -1,7 +1,10 @@
+# load libraries/data
 library(survival)
 library(shiny)
 load("validation.data.mRNA.RData")
 load("GI.interactions.RData")
+
+# initial data formatting
 clin <- clinical[,17:18]
 rm(clinical)
 data <- cbind2(data.matrix(clin),t(measurements))
@@ -10,5 +13,17 @@ interactions <- dataset$states
 rm(dataset)
 formula <- with(clin,Surv(time,status==1))
 fullKm <- survfit(formula~1,data=clin,conf.type="log-log")
-rm(formula)
+rm(formula,clin)
+
+# create role table
+load("driver.states.RData")
+roles <- cbind2(dataset[,1:2],dataset[,6:7])
+rm(dataset)
+roles <- subset(roles,roles[,3]!="" | roles[,4]!="")
+names <- c(roles[,1],roles[,2])
+roles <- c(roles[,3],roles[,4])
+roles <- unique(cbind2(names,roles))
+roles <- subset(roles,roles[,2]!="")
+rm(names)
+
 runApp("../CancerApp",host="128.8.132.200",port=2017)
